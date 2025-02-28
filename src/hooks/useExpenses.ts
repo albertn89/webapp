8e45dc8a5d243905cbb58b62d8303e6a9bb6d1c7
Albert Negoro <albert.negoro89@gmail.com>
2025-02-28 01:31:38 -0700

    completed the read expenses

---
+++ src/hooks/useExpenses.ts
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import { Expense } from "../model/Expense";
+import { getExpenses } from "../services/expense-service";
+
+const useExpenses = () => {
+	const [expenses, setExpenses] = useState<Expense[]>([]);
+	const [error, setError] = useState(null);
+	const [isLoading, setIsLoading] = useState(false);
+
+	useEffect(() => {
+		// api call to backend system
+		setIsLoading(true);
+		getExpenses()
+			.then((response) => setExpenses(response.data))
+			.catch((error) => setError(error))
+			.finally(() => setIsLoading(false));
+	}, []);
+
+	return { expenses, error, isLoading };
+};
+
+export default useExpenses;
